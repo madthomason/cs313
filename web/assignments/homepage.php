94484ddd35ad09f447d10a11f56bf9c9eae45e89
madthomason <tho16031@byui.edu>
2019-01-18 14:19:14 -0700

    trying out bootstrap

--- web/assignments/homepage.php
+++ web/assignments/homepage.php
@@ -6,7 +6,8 @@
     <title>Homepage</title>
     <link rel="stylesheet" href="general.css">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 </head>
 <body>
@@ -15,30 +16,33 @@
     <a href="assignments.html"></a>
 </div>
 <div class="main">
-    <div>
-        <h2>Madeline Barlocker</h2>
-        <img src="media/IMG_5398.png" alt="Sand Hill, Lake Powell">
-        <p class="caption">I love the annual Lake Powell trip with my in-laws. A week free of cares on the lake with
-            family,
-            nothing better.</p>
-
-        <h3>A Few Fun Facts About Me</h3>
-        <p>I am from a small town in NE Washington called Chewelah (think 2500 people).
-            I just finished my internship with the Temple Portfolio in the ICS department of the church in Riverton.
-            I was called to serve in the London South mission but having gone on a blind date(set up by his senior
-            project which is a whole other story) the day my papers went in
-            I ended up ditching the mission for a different eternal M with my partner in crime Garrett.
-            I love to cook, and then eat said cooking.
-            This is my last semester of my Software Engineering major which will be followed closely by the addition of
-            a little girl to our family!
-        </p>
+    <div class="well">
+        <div>
+            <h3>A Few Fun Facts About Madeline Barlocker:</h3>
+            <p>I am from a small town in NE Washington called Chewelah (think 2500 people).
+                I just finished my internship with the Temple Portfolio in the ICS department of the church in Riverton.
+                I was called to serve in the London South mission but having gone on a blind date(set up by his senior
+                project which is a whole other story) the day my papers went in
+                I ended up ditching the mission for a different eternal M with my partner in crime Garrett.
+                I love to cook, and then eat said cooking.
+                This is my last semester of my Software Engineering major which will be followed closely by the addition
+                of
+                a little girl to our family!
+            </p>
+        </div>
+        <div>
+            <img src="media/IMG_5398.png" class="img-rounded" alt="Sand Hill, Lake Powell">
+            <p class="figure-caption text-center text-wrap">I love the annual Lake Powell trip with my in-laws. A week free of cares on the lake with
+                family,
+                nothing better.</p>
+        </div>
     </div>
     <div>
-        <video id="video">
+        <video id="video" width="320" height="240" controls>
             <source src="media/IMG_3231.mp4" type="video/mp4">
             Your Browser doesn't support the video tag
         </video>
-        <p class="caption">Hover to play the video</p>
+        <p class="text-muted">Hover to play the video</p>
     </div>
 </div>
 <div class="countdown">
