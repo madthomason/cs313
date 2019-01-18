a6f7270b480800095d3b5554cca2a58c06e63846
madthomason <tho16031@byui.edu>
2019-01-18 14:51:34 -0700

    display flex

--- web/assignments/homepage.php
+++ web/assignments/homepage.php
@@ -11,47 +11,54 @@
 
 </head>
 <body>
-<div class="header d-flex flex-row">
+<div class="header d-flex justify-content-around">
     <h1>About Me!</h1>
-    <a href="assignments.html"></a>
-    <div class="countdown bg-success">
-        <?php
-        $today = time();
-        $graduation = mktime(0, 0, 0, 4, 12, 2019);
-        $days = ($graduation - $today) / 86400;
-        echo "<h2>$days days until graduation, but who's counting...</h2>";
+    <div class="d-flex justify-content-around">
+        <a href="assignments.html"><h2>Assignments</h2></a>
+        <div class="countdown">
+            <?php
+            $today = time();
+            $graduation = mktime(0, 0, 0, 4, 12, 2019);
+            $days = floor(($graduation - $today) / 86400);
+            echo "<h2>$days days until graduation, but who's counting...</h2>";
 
-        ?>
+            ?>
+        </div>
     </div>
 </div>
-<div class="main bg-info">
-    <div class="well d-flex flex-row">
-        <div>
-            <h3>A Few Fun Facts About Madeline Barlocker:</h3>
-            <ul>
-                <li>I am from a small town in NE Washington called Chewelah (think 2500 people).</li>
-                <li>I just finished my internship with the Temple Portfolio in the ICS department of the church in Riverton.</li>
-                <li> I was called to serve in the London South mission but having gone on a blind date(set up by his senior
-                    project which is a whole other story) the day my papers went in
-                    I ended up ditching the mission for a different eternal M with my partner in crime Garrett.</li>
-                <li>I love to cook, and then eat said cooking.</li>
-                <li>This is my last semester of my Software Engineering major which will be followed closely by the addition
-                    of a little girl to our family!</li>
-            </ul>
-        </div>
-        <div>
-            <img src="media/IMG_5398.png" class="img-rounded" alt="Sand Hill, Lake Powell">
-            <p class="figure-caption text-center text-wrap">I love the annual Lake Powell trip with my in-laws. A week free of cares on the lake with
-                family,
-                nothing better.</p>
-        </div>
+<div class="well d-flex flex-row bg-info">
+    <div>
+        <h3>A Few Fun Facts About Madeline Barlocker:</h3>
+        <ul>
+            <li>I am from a small town in NE Washington called <a href="https://www.cityofchewelah.org/">Chewelah</a>(think
+                2500 people).
+            </li>
+            <li>I just finished my internship with the Temple Portfolio in the ICS department of the church in
+                Riverton.
+            </li>
+            <li> I was called to serve in the London South mission but having gone on a blind date(set up by his senior
+                project which is a whole other story) the day my papers went in
+                I ended up ditching the mission for a different eternal M with my partner in crime Garrett.
+            </li>
+            <li>I love to cook, and then eat said cooking.</li>
+            <li>This is my last semester of my Software Engineering major which will be followed closely by the addition
+                of a little girl to our family!
+                <div>
+                    <video id="video" width="320" height="240" controls>
+                        <source src="media/IMG_3231.mp4" type="video/mp4">
+                        Your Browser doesn't support the video tag
+                    </video>
+                    <p class="text-muted">Hover to play the video</p>
+                </div>
+            </li>
+        </ul>
     </div>
     <div>
-        <video id="video" width="320" height="240" controls>
-            <source src="media/IMG_3231.mp4" type="video/mp4">
-            Your Browser doesn't support the video tag
-        </video>
-        <p class="text-muted">Hover to play the video</p>
+        <img src="media/IMG_5398.png" class="img-rounded" alt="Sand Hill, Lake Powell">
+        <p class="figure-caption text-center text-wrap">I love the annual Lake Powell trip with my in-laws. A week free
+            of cares on the lake with
+            family,
+            nothing better.</p>
     </div>
 </div>
 
