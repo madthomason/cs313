6ab7ed70f54e89d38f9700be66e74cca8b20aec3
madthomason <tho16031@byui.edu>
2019-01-19 10:28:03 -0700

    new cards approach

--- web/assignments/homepage.php
+++ web/assignments/homepage.php
@@ -10,65 +10,105 @@
           integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 </head>
-<body class="bg-light">
-    <div class="jumbotron bg-secondary d-flex justify-content-around">
-        <h1>About Me!</h1>
-        <!--    <div class="d-flex justify-content-around">-->
-        <a href="assignments.html"><h2>Assignments</h2></a>
-        <div class="countdown">
-            <?php
-            $today = time();
-            $graduation = mktime(0, 0, 0, 4, 12, 2019);
-            $days = floor(($graduation - $today) / 86400);
-            echo "<h2>$days days until graduation, but who's counting...</h2>";
+<body class="bg-info">
+<div class="jumbotron bg-secondary d-flex justify-content-around">
+    <h1>About Me!</h1>
+    <!--    <div class="d-flex justify-content-around">-->
+    <a href="assignments.html"><h2>Assignments</h2></a>
+    <div class="countdown">
+        <?php
+        $today = time();
+        $graduation = mktime(0, 0, 0, 4, 12, 2019);
+        $days = floor(($graduation - $today) / 86400);
+        echo "<h2>$days days until graduation, but who's counting...</h2>";
 
-            ?>
-        </div>
-        <!--    </div>-->
+        ?>
     </div>
-    <div class="jumbotron d-flex justify-content-around bg-info row">
-        <div class="col-6 bg-secondary">
-            <h3>A Few Fun Facts About Madeline Barlocker:</h3>
-            <ul>
-                <li class="h5">I am from a small town in NE Washington(think
-                    2500 people) called <a href="https://www.cityofchewelah.org/">Chewelah</a>.
-                </li>
-                <li class="h5">I just finished my internship with the Temple Portfolio in the ICS department of the church in
-                    Riverton.
-                </li>
-                <li class="h5"> I was called to serve in the London South mission but having gone on a blind date(set up by his
-                    senior
-                    project which is a whole other story) the day my papers went in
-                    I ended up ditching the mission for a different eternal M with my partner in crime Garrett.
-                </li>
-                <li class="h5">I love to cook, and then eat said cooking.</li>
-                <li>
-                    <div class="d-flex">
-                        <p class="text-wrap h5">This is my last semester of my Software Engineering major which will be followed closely by
-                            the
-                            addition of a little girl to our family!
-                        </p>
+    <!--    </div>-->
+</div>
+<div class="jumbotron">
+        <h3>A Few Fun Facts About Madeline Barlocker:</h3>
 
-                        <div>
-                            <video id="video" width="320" height="180" preload="auto">
-                                <source src="media/IMG_3231.mp4" type="video/mp4">
-                                Your Browser doesn't support the video tag
-                            </video>
-                            <p class="font-italic text-center">Hover to play the video</p>
-                        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="media/IMG_5398.png" class="img-rounded" alt="Sand Hill, Lake Powell">
+                    <div class="card-body">
+                        <h5 class="card-title">Lake Powell</h5>
+                        <p class="card-text">I love the annual Lake Powell trip with my in-laws. A week
+                            free of cares on the lake with family, nothing better.</p>
                     </div>
-                </li>
-            </ul>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="http://mw2.google.com/mw-panoramio/photos/medium/1482110.jpg" class="card-img-top"
+                         alt="Chewelah, WA">
+                    <div class="card-body">
+                        <h5 class="card-title">Home</h5>
+                        <p class="card-text">I am from a small town in NE Washington(think
+                            2500 people) called Chewelah.</p>
+                        <a href="https://www.cityofchewelah.org/" class="btn btn-primary">Checkout Chewelah!</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="jumbotron col-4 bg-secondary p-2">
-            <img src="media/IMG_5398.png" class="img-rounded" alt="Sand Hill, Lake Powell">
-            <p class="font-italic text-center text-wrap">I love the annual Lake Powell trip with my in-laws. A week
-                free
-                of cares on the lake with
-                family,
-                nothing better.</p>
+
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="media/Wedding-0255.jpg" class="card-img-top"
+                         alt="12/27/17">
+                    <div class="card-body">
+                        <p class="card-text">I was called to serve in the London South mission but having gone on a
+                            blind date(set up by his
+                            senior project which is a whole other story) the day my papers went in
+                            I ended up ditching the mission for a different eternal M with my partner in crime
+                            Garrett.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <img src="https://render.fineartamerica.com/images/rendered/search/print/images/artworkimages/medium/1/oquirrh-mountain-temple-sunset-john-wunderli.jpg" class="card-img-top"
+                         alt="Oquirr Mountain Temple, South Jordan">
+                    <div class="card-body">
+                        <h5 class="card-title">Temple Portfolio Internship</h5>
+                        <p class="card-text">I just finished my internship with the Temple Portfolio in the ICS
+                            department of the church in Riverton working on the recording software for the temples.</p>
+                        <a href="https://www.linkedin.com/in/madeline-barlocker-03484b149" class="btn btn-primary">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">My true passion</h5>
+                        <p class="card-text">I love to cook, and then eat said cooking.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <video class="card-img-top" id="video" width="320" height="180" preload="auto">
+                        <source src="media/IMG_3231.mp4" type="video/mp4">
+                        Your Browser doesn't support the video tag
+                    </video>
+                    <div class="card-body">
+                        <p class="font-italic text-center card-text">Hover to play the video</p>
+                        <h5 class="card-title">Endings and New Beginnings</h5>
+                        <p class="card-text">This is my last semester of my Software Engineering major which will be
+                            followed closely by the addition of a little girl to our family!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
 <script>
     const video = document.getElementById("video");
     video.addEventListener('mouseover', function (e) {
