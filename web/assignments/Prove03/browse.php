8778447a49eb22bf4d4592f17247de136013f8b4
madthomason <tho16031@byui.edu>
2019-01-23 19:01:54 -0700

    items adding to cart

--- web/assignments/Prove03/browse.php
+++ web/assignments/Prove03/browse.php
@@ -6,13 +6,18 @@ require 'header.php';
 
 <div class="row w-100 m-0 p-3">
     <?php
+
      foreach ($titles as $x => $title) {
+         $button_class = "btn bg-primary";
+         if (in_array($x, $_SESSION["cart_items"])) {
+             $button_class .= "disabled";
+         }
          echo '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 p-1">
                     <div class="card h-100">
                         <img src="' . $images[$x] . '" class="card-img-top">
                         <div class="d-flex justify-content-around card-body">
                             <h5 class="card-title">' . $title . '</h5>
-                            <a class="btn bg-primary" href="add_items.php">
+                            <a class="btn bg-primary" href="add_items.php?id=' . $x . '">
                                 <i class="fas fa-cart-plus"></i>
                             </a>
                         </div>
