9592278b1a7cc5a86adb4cb50ad59d10f3eabae5
madthomason <tho16031@byui.edu>
2019-01-23 19:10:14 -0700

    items.php fixing

--- web/assignments/Prove03/add_items.php
+++ web/assignments/Prove03/add_items.php
@@ -15,7 +15,12 @@ if (!isset($_SESSION["cartItems"])) {
 }
 
 if ($_GET["id"]) {
-    $_SESSION["cartItems"] .= ", " . $_GET["id"];
+    if (!empty($_SESSION["cartItems"])) {
+        $_SESSION["cartItems"] .= ", " . $_GET["id"];
+    } else {
+        $_SESSION["cartItems"] =  $_GET["id"];
+    }
+    
 }
 
 
