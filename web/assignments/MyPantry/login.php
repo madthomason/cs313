8e00611fda2ac66bbffac3af1a4d2d3b7fb937ca
madthomason <tho16031@byui.edu>
2019-02-09 10:10:38 -0700

    login css

--- web/assignments/MyPantry/login.php
+++ web/assignments/MyPantry/login.php
@@ -26,7 +26,7 @@ if (isset($_GET["error"]) || $error) {
 //$signUpStmt->execute(array(':name' => $name, ':email' => $email));
 
 ?>
-    <div class="">
+    <div class="d-flex justify-content-center align-items-center vh-100">
         <div class="card h-100 p-3">
             <form class="form-signin" action="login.php" method="post">
                 <h2>Please login</h2>
