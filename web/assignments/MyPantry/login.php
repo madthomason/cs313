eb1da44ca1b33121ff14503cb804f420ad0e7de3
madthomason <tho16031@byui.edu>
2019-02-09 10:18:20 -0700

    login vert align

--- web/assignments/MyPantry/login.php
+++ web/assignments/MyPantry/login.php
@@ -26,9 +26,8 @@ if (isset($_GET["error"]) || $error) {
 //$signUpStmt->execute(array(':name' => $name, ':email' => $email));
 
 ?>
-<div class="vertical-align">
-    <div class="d-flex justify-content-center align-items-center">
-        <div class="card h-100 p-3">
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="card p-3">
             <form class="form-signin" action="login.php" method="post">
                 <h2>Please login</h2>
                 <input type="text" class="form-control" name="username" placeholder="Email Address or Username"
@@ -46,7 +45,6 @@ if (isset($_GET["error"]) || $error) {
         <!--        </form>-->
         <!--    </div>-->
     </div>
-</div>
 </body>
 </html>
 
