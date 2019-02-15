5fbe132efd53a084973820062d577e73b9ef6c6a
madthomason <tho16031@byui.edu>
2019-02-15 11:26:38 -0700

    signup page error messages

--- web/assignments/MyPantry/login.php
+++ web/assignments/MyPantry/login.php
@@ -7,7 +7,11 @@ if (isset($_GET["signup"])) {
             header("Location: pantry.php?id=" . $userId);
         } else {
             $error = true;
+            $message = "Error in sign up. Please Try again";
         }
+    } else {
+        $error = true;
+        $message = "Missing information in sign up. Try again";
     }
 } else {
     if (isset($_POST["username"]) && isset($_POST["password"])) {
@@ -16,7 +20,11 @@ if (isset($_GET["signup"])) {
             header("Location: pantry.php?id=" . $userId);
         } else {
             $error = true;
+            $message = "Incorrect Username/Password. Try again";
         }
+    }else {
+        $error = true;
+        $message = "Incorrect Username/Password. Try again";
     }
 }
 
@@ -24,7 +32,6 @@ if (isset($_GET["signup"])) {
 
 if (isset($_GET["error"]) || $error) {
     //alert error
-    $message = "Incorrect Username/Password. Try again";
     echo "<script type='text/javascript'>alert('$message');</script>";
 }
 
@@ -39,27 +46,27 @@ if (isset($_GET["error"]) || $error) {
         <form class="form-signin" action="login.php" method="post">
             <h2>Please login</h2>
             <input type="text" class="form-control" name="username" placeholder="Email Address or Username"
-                   required="" autofocus=""/>
-            <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+                   autofocus="" required/>
+            <input type="password" class="form-control" name="password" placeholder="Password" required/>
             <input class="btn btn-lg btn-primary btn-block" type="submit" value="Login">
         </form>
         <h6>Don't have an account?</h6>
-        <button class="btn btn-lg btn-primary btn-block" onclick="toggleCards()">Sign Up!</button>
+        <button class="btn btn-md btn-primary btn-block" onclick="toggleCards()">Sign Up!</button>
     </div>
 
     <div id="signup" class="card w-25 p-3 d-none">
         <form class="form-signin" action="login.php?signup=true" method="post">
             <h2>Sign Up</h2>
             <input type="text" class="form-control" name="username" placeholder="Username"
-                   required="" autofocus=""/>
+                   autofocus="" required/>
             <input type="email" class="form-control" name="email" placeholder="Email Address"
-                   required="" autofocus=""/>
+                   autofocus=""  required/>
 
-            <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+            <input type="password" class="form-control" name="password" placeholder="Password" required/>
             <input class="btn btn-lg btn-primary btn-block" type="submit" value="Sign Up">
         </form>
         <h6>Already have an account?</h6>
-        <button class="btn btn-lg btn-primary btn-block" onclick="toggleCards()">Login!</button>
+        <button class="btn btn-md btn-primary btn-block" onclick="toggleCards()">Login!</button>
     </div>
 </div>
 <script>
