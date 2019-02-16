f559aab01007cab52c3bebf28f35d39758b26cb2
madthomason <tho16031@byui.edu>
2019-02-16 16:36:26 -0700

    added bootstrap js and jquery

--- web/assignments/MyPantry/header.php
+++ web/assignments/MyPantry/header.php
@@ -16,13 +16,13 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Pantry</title>
-    <link rel="stylesheet" href="../general.css">
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
           integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
           integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../general.css">
 </head>
 <body class="bg-info">
