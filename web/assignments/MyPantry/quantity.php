9766d65141b3a51ce8b2dc26532830e9d0924cf2
madthomason <tho16031@byui.edu>
2019-02-25 15:03:09 -0700

    adding sendgrid email

--- web/assignments/MyPantry/quantity.php
+++ web/assignments/MyPantry/quantity.php
@@ -6,6 +6,7 @@
  * Time: 5:54 PM
  */
 require_once 'database.php';
+require("../../../sendgrid-php/sendgrid-php.php");
 $db = getDb();
 session_start();
 $msg = "sent!";
@@ -20,13 +21,40 @@ if (isset($_GET["id"])) {
             $updateItemsStmt->bindParam(":id", $_GET["id"], PDO::PARAM_INT);
             $updateItemsStmt->execute();
             //$msg = emailNotifications($_SESSION["user"], $db);
-            $sent = mail($_SESSION["user"]["email"],"Some Items Need Restocking", $msg);
+            $email = new \SendGrid\Mail\Mail();
+            $email->addTo($_SESSION["user"]["email"])
+                ->setFrom("tho16031@byui.edu")
+                ->setSubject("Hello World from the SendGrid PHP Library!")
+                ->setText("Hello, Email!");
+            
+            $sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+            $response = $sg->send($email);
             if (!$sent) {
                 $msg = "notSent email " . $_SESSION["user"]["email"];
             }
         }
     }
 }
+
+function email() {
+
+
+    $from = new SendGrid\Email(null, "test@example.com");
+    $subject = "Hello World from the SendGrid PHP Library!";
+    $to = new SendGrid\Email(null, "test@example.com");
+    $content = new SendGrid\Content("text/plain", "Hello, Email!");
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
+
+    $response = $sg->client->mail()->send()->post($mail);
+    echo $response->statusCode();
+    echo $response->headers();
+    echo $response->body();
+}
+
 flush();
 header("Location: pantry.php?msg=" . $msg);
 die();
