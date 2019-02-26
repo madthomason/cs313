a31df23fb658f1677f57d7c3b75ce6e0c8096677
madthomason <tho16031@byui.edu>
2019-02-26 09:49:42 -0700

    getting rid of debugging

--- web/assignments/MyPantry/pantry.php
+++ web/assignments/MyPantry/pantry.php
@@ -16,9 +16,9 @@ if (isset($_SESSION["user"]) && !isset($_GET["cupboardId"])) {
 
     //Notify Users if their items are in need of restocking
     $message =  addslashes(emailNotifications($_SESSION["user"], $db));
-   // if ($message !== "empty") {
+    if ($message !== "empty") {
         echo "<script>alert(\"$message\");</script>";
-    //}
+    }
 
 } else if (isset($_GET["cupboardId"])) {
     $items = getItems($_GET["cupboardId"], $db);
